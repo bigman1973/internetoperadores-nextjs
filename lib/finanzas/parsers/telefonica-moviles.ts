eccc1664e66f70dc859cd8d578fc18a6f4316489
IO Dev <dev@internetoperadores.com>
2026-06-30 08:08:19 +0000

    fix: polyfill DOMMatrix para pdfjs-dist en Vercel serverless

--- lib/finanzas/parsers/telefonica-moviles.ts
+++ lib/finanzas/parsers/telefonica-moviles.ts
@@ -13,6 +13,31 @@
 
 import { prisma } from '@/lib/prisma';
 
+// Polyfill DOMMatrix para entornos serverless (Vercel) donde no existe DOM
+if (typeof globalThis.DOMMatrix === 'undefined') {
+  (globalThis as any).DOMMatrix = class DOMMatrix {
+    a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+    m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+    m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+    m31 = 0; m32 = 0; m33 = 1; m34 = 0;
+    m41 = 0; m42 = 0; m43 = 0; m44 = 1;
+    is2D = true; isIdentity = true;
+    constructor(init?: any) {
+      if (Array.isArray(init) && init.length === 6) {
+        [this.a, this.b, this.c, this.d, this.e, this.f] = init;
+        this.m11 = this.a; this.m12 = this.b;
+        this.m21 = this.c; this.m22 = this.d;
+        this.m41 = this.e; this.m42 = this.f;
+      }
+    }
+    inverse() { return new DOMMatrix(); }
+    multiply() { return new DOMMatrix(); }
+    scale() { return new DOMMatrix(); }
+    translate() { return new DOMMatrix(); }
+    transformPoint(p: any) { return p || { x: 0, y: 0, z: 0, w: 1 }; }
+  };
+}
+
 export interface LineaTelefonica {
   extension: string;
   telefono: string;
